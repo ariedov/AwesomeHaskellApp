f4693fbffcd242d9d29764aa624b8d9f2b9b738e
David <ariedov@gmail.com>
2013-11-18 22:20:16 +0200

    added code to main.hs

--- main.hs
+++ main.hs
@@ -1,1 +1,7 @@
 doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+			then x
+			else x * 2
+doubleSmallNumber' x = doubleSmallNumber x + 1
+conanO'brian = "Hello, this is Conan O'Brian"
