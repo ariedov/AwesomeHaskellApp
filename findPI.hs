8b9107eb66c59c283bf837ddd8c2e1d421ee3cc8
David <ariedov@gmail.com>
2013-11-19 21:59:20 +0200

    pi search function in Haskell

---
+++ findPI.hs
@@ -0,0 +1,6 @@
+findPI x = sum [4 / if r `elem` [3,7..x*4]
+		then r * (-1) 
+		else r 
+		| 
+		r <- [1,3..x], 
+		x > 3]
--- main.hs
+++ main.hs
@@ -5,3 +5,10 @@ doubleSmallNumber x = if x > 100
 			else x * 2
 doubleSmallNumber' x = doubleSmallNumber x + 1
 conanO'brian = "Hello, this is Conan O'Brian"
+tenEvenNumbers = [x*2 | x <- [1..10]]
+threeMoreFromSeven = [x | x <- [50..100], x `mod` 7 == 3]
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+length' xs = sum [ 1 | _ <- xs]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]  
+
