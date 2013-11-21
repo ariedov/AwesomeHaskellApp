05dae45c061afd139549962fe4b8204f2b9216ae
David <ariedov@gmail.com>
2013-11-21 21:30:58 +0200

    learning recursion

--- main.hs
+++ main.hs
@@ -12,3 +12,36 @@ length' xs = sum [ 1 | _ <- xs]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]  
 
+head' (x:_) = x
+
+length'' [] = 0
+length'' (_:xs) = 1 + length' xs
+
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+	| x > maxTail =x
+	| otherwise = maxTail
+	where maxTail = maximum' xs
+
+replicate' n x 
+	| n <= 0 = []
+	| otherwise = x:replicate' (n-1) x
+
+take' n _
+	| n <= 0 = []
+take' _ [] 	 = []	
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+repeat' x = x:repeat' x
+
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' a [] = False
+elem' a (x:xs)
+	| a == x = True
+	| otherwise = a `elem'` xs
