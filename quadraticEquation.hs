eb0fca49449969ebb5c6a0824d2b4459dbeb4d21
David <ariedov@gmail.com>
2013-11-23 18:28:02 +0200

    handling d < 0

--- quadraticEquation.hs
+++ quadraticEquation.hs
@@ -1,9 +1,14 @@
 solveQuadratic :: Float -> Float -> Float -> (Float, Float)
-solveQuadratic a b c = (x1, x2) 
-	where   d  = findD a b c
-		x1 = ((-b) + (sqrt d)) / (2 * a)
-		x2 = ((-b) - (sqrt d)) / (2 * a)
+solveQuadratic a b c = solve a b $ findD a b c
 
 findD :: Float -> Float -> Float -> Float
 findD a b c = (b^2) - (4 * a * c)
-  
+
+solve :: Float -> Float -> Float -> (Float, Float)
+solve a b d
+	| d >= 0	= (x1, x2)
+	| d < 0		= error "There is no solution"
+	where 	x1 = ((-b) + (sqrt d)) / (2 * a)
+		x2 = ((-b) - (sqrt d)) / (2 * a)
+
+ 	
